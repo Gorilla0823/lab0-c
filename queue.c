42b0763948aa46932b9f538aad09c34910e1c98e
Gorilla0823 <gorilla@csie.io>
2020-03-20 06:17:28 +0800

    Refine q_sort

--- queue.c
+++ queue.c
@@ -230,18 +230,6 @@ void q_reverse(queue_t *q)
  */
 static list_ele_t *merge(list_ele_t *l1, list_ele_t *l2)
 {
-    // merge with recursive
-    /*if (!l2)
-        return l1;
-    if (!l1)
-        return l2;
-    if (strnatcmp(l1->value, l2->value) < 0) {
-        l1->next = merge(l1->next, l2);
-        return l1;
-    } else {
-        l2->next = merge(l1, l2->next);
-        return l2;
-    }*/
     list_ele_t *temp = NULL;
     list_ele_t **p = &temp;
 
@@ -282,7 +270,7 @@ static list_ele_t *mergeSortList(list_ele_t *head)
 
     list_ele_t *l1 = mergeSortList(head);
     list_ele_t *l2 = mergeSortList(fast);
-    // merge sorted l1 and sorted l2
+
     return merge(l1, l2);
 }
 void q_sort(queue_t *q)
